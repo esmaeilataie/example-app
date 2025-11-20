59371f202b3c782304e2ddd75b419b887c431039
Esmaeil Ataie <esmaeilataie@yahoo.com>
2025-11-20 16:39:34 +0330

    day-6

---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <x-slot:heading>
+        Jobs Page
+    </x-slot:heading>
+    <ul>
+        @foreach ($jobs as $job)
+            <a href="jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
+                <li> <strong>{{ $job['title'] }}</strong> : pays {{ $job['salary'] }} per year.</li>
+            </a>
+        @endforeach
+    </ul>
+</x-layout>
