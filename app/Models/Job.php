7555dc744db15ef0dab593639d0bc8e258c8b84a
Esmaeil Ataie <esmaeilataie@yahoo.com>
2025-11-24 18:43:35 +0330

    day-9

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,41 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-       return [
-            [
-                'id' => 1,
-                'title' => 'Directore',
-                'salary' => '$50,000',
-            ],
+    protected $table = 'job_listings';
 
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000',
-            ],
-
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000',
-            ],
-        ];
-    }
-
-    public static function find($id): array
-    {
-        $job = Arr::first(static::all(),fn($job) => $job['id'] == $id);
-        
-        if(! $job){
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
