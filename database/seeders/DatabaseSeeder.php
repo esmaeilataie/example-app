822b833024255f1080f03a197cd567bea2a06681
Esmaeil Ataie <esmaeilataie@yahoo.com>
2025-11-25 20:34:40 +0330

    day-15

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+         User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'first_name' => 'John',
+//            'last_name' => 'Doe',
+//            'email' => 'test@example.com',
+//        ]);
+
+//        Job::factory(20)->create();
+        $this->call(JobSeeder::class);
     }
 }
