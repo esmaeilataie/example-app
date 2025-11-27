3dce81515cf66a5b4eee2cc636aaa80a1c452203
Esmaeil Ataie <esmaeilataie@yahoo.com>
2025-11-27 12:36:36 +0330

    day-18

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,6 +2,9 @@
     <x-slot:heading>
         Job
     </x-slot:heading>
-    <h1 class="font-bold text-lg">{{$job['title']}}</h1>
-    <p>This job pays, {{$job['salary']}} per year.</p>
+    <h1 class="font-bold text-lg">{{$job->title}}</h1>
+    <p>This job pays, {{$job->salary}} per year.</p>
+    <p class="mt-6">
+        <x-button href="/jobs/{{$job->id}}/edit">Edit Job</x-button>
+    </p>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
     return view('jobs.index', [
@@ -14,6 +15,7 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Store
 Route::post('/jobs',function(){
     //validation...
     request()->validate([
@@ -30,16 +32,50 @@ Route::post('/jobs',function(){
     return redirect('/jobs');
 });
 
+//Create Job
 Route::get('/jobs/create',function(){
    return view('jobs.create');
 });
 
-
+// Show
 Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
 
     return view('jobs.show',['job' => $job]);
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+
+    $job = Job::find($id);
+
+    return view('jobs.edit',['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+
+    request()->validate([
+        'title' => ['required','min:3'],
+        'salary' => ['required']
+    ]);
+    // authorize (On hold...)
+    $job = Job::findOrFail($id);
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    // authorize (On hold...)
+    Job::findOrFail($id)->delete();
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
