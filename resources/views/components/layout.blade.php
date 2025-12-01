817ab774a51cf1903993671025f14e483c6320ed
Esmaeil Ataie <esmaeilataie@yahoo.com>
2025-12-01 15:42:43 +0330

    day-26

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Website</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/js/app.js','resources/css/app.css'])
+{{--    <script src="https://cdn.tailwindcss.com"></script>--}}
 
 </head>
 
